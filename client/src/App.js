44beb6ada4ab00414eb5a23b43b5371561a9686f
Kavinda Bandara Medagoda <108758296+Kavinda-B-Medagoda@users.noreply.github.com>
2023-07-24 21:09:08 +0530

    two navbars added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
-import Navbar_1 from './components/Navbar_1';
+import "./App.css";
+import Navbar_2 from "./components/Navbar_2";
+// import Navbar_1 from "./components/Navbar_1";
 
 function App() {
   return (
-    <div className="App bg-[#F5F5F5]" >
-      <div className='top w-full'></div>
-      <header className='header'>
-        <Navbar_1/>
+    <div className="App bg-[#F5F5F5]">
+      <div className="top w-full"></div>
+      <header className="header">
+        {/* <Navbar_1 /> */}
+        <Navbar_2 />
       </header>
     </div>
   );
