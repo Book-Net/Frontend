ce159a203100a390a2176f5f248f6e621891535f
Kavinda Bandara Medagoda <108758296+Kavinda-B-Medagoda@users.noreply.github.com>
2023-07-26 06:37:37 +0530

    signup form done

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
-import Main_image from "./components/Main_image";
-import Navbar_2 from "./components/Navbar_2";
-// import Navbar_1 from "./components/Navbar_1";
+// import Main_image from "./components/Main_image";
+// import Navbar_2 from "./components/Navbar_2";
+import Navbar_1 from "./components/Navbar_1";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App bg-[#F5F5F5]">
       <div className="top w-full fixed top-0"></div>
       <header className="header">
-        {/* <Navbar_1 /> */}
-        <Navbar_2 />
+        <Navbar_1 />
+        {/* <Navbar_2 /> */}
       </header>
       <main className="mt-[120px]">
-        <Main_image />
+        {/* <Main_image /> */}
+        <Signup />
       </main>
     </div>
   );
