3effa5f879fdf10e7ae864789da9870d80730105
lahiru sanjana <lahirusanjana1@gmail.com>
2023-08-09 16:32:41 +0530

    profile navbar changed

--- client/src/components/Profile_navbar.js
+++ client/src/components/Profile_navbar.js
@@ -5,10 +5,10 @@ function Profile_navbar() {
   return (
     <div>
         <div className='font-roboto text-xl text-[#BF5A36] font-semibold block md:w-[1400px] md:flex'>
-            <div className=' my-1 mx-0 min-w-[250px] hover:border-r-4 hover:border-b-0 hover:border-t-4 transition-all duration-100 rounded-tr-lg px-4 py-2 border-b-4 border-[#BF5A36]'><Link to='./' className=''>Purchase History</Link></div>
-            <div className=' my-1 mx-0 min-w-[250px] hover:border-r-4 hover:border-b-0 hover:border-l-4 hover:border-t-4 transition-all duration-100 rounded-t-lg px-4 py-2 border-b-4 border-[#BF5A36]'><Link to='./' className=''>Favourites</Link></div>
-            <div className=' my-1 mx-0 min-w-[250px] hover:border-r-4 hover:border-b-0 hover:border-l-4 hover:border-t-4 transition-all duration-100 rounded-t-lg px-4 py-2 border-b-4 border-[#BF5A36]'><Link to='./' className=''>Settings</Link></div>
-            <div className=' my-1 mx-0 min-w-[250px] hover:border-b-0 hover:border-l-4 hover:border-t-4 transition-all duration-100 rounded-tl-lg px-4 py-2 border-b-4 border-[#BF5A36]'><Link to='./' className=''>Dashboard</Link></div>
+            <div className=' my-1 mx-0 min-w-[250px] hover:border-r-2 hover:border-b-0 hover:border-t-2 transition-all duration-100 rounded-tr-lg px-4 py-2 border-b-2 border-[#BF5A36]'><Link to='./' className=''>Purchase History</Link></div>
+            <div className=' my-1 mx-0 min-w-[250px] hover:border-r-2 hover:border-b-0 hover:border-l-2 hover:border-t-2 transition-all duration-100 rounded-t-lg px-4 py-2 border-b-2 border-[#BF5A36]'><Link to='./' className=''>Favourites</Link></div>
+            <div className=' my-1 mx-0 min-w-[250px] hover:border-r-2 hover:border-b-0 hover:border-l-2 hover:border-t-2 transition-all duration-100 rounded-t-lg px-4 py-2 border-b-2 border-[#BF5A36]'><Link to='./' className=''>Settings</Link></div>
+            <div className=' my-1 mx-0 min-w-[250px] hover:border-b-0 hover:border-l-2 hover:border-t-2 transition-all duration-100 rounded-tl-lg px-4 py-2 border-b-2 border-[#BF5A36]'><Link to='./' className=''>Dashboard</Link></div>
         </div>
     </div>
   )
