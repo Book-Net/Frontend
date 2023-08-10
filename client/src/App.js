e3ad204d7ada45274a2f682ec2f7eabb02ac6e31
Samitha <rathnayakesamitha1317@gmail.com>
2023-08-10 08:04:50 +0530

    bid_sell

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import { createContext, useState } from "react";
 import Add_book_for_sell from "./components/Add_book_for_sell";
 import Add_book_for_bid from "./components/Add_book_for_bid";
+import Bid_Sell from "./components/Bid_Sell";
 
 function App() {
   const isLoggedIn = false; //change this after authentication done
@@ -19,19 +20,12 @@ function App() {
 
   return (
     <div className="App bg-[#F5F5F5]">
-      <div className="top w-full fixed top-0"></div>
+      <div className="fixed top-0 w-full top"></div>
       <header className="header">
         {isLoggedIn ? <Navbar_2 /> : <Navbar_1 />}
       </header>
-      <main className="mt-[120px]">
-        <Routes>
-          <Route path="/" element={<Add_book_for_bid />} />
-          <Route path="/login" element={<Login />} />
-          {/* If you meant /signUp instead of /signup, change the path accordingly */}
-          <Route path="/signup" element={<Signup />} />
-          {/* Redirect to home page if no matching route is found */}
-          <Route path="*" element={<Home />} />
-        </Routes>
+      <main>
+        <Bid_Sell/>
       </main>
     </div>
   );
