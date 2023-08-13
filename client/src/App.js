6d8ea94bab2afeea375475eb8860da1fd277102d
Kavinda Bandara Medagoda <108758296+Kavinda-B-Medagoda@users.noreply.github.com>
2023-08-13 11:53:06 +0530

    backend connected

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar_1 from "./components/Navbar_1";
 import Navbar_2 from "./components/Navbar_2";
 import Signup from "./pages/Signup";
-import Reg_home from "./pages/Reg_home";
-import Login from "./pages/Login";
-import { createContext, useState } from "react";
-import Unreg_home from "./pages/Unreg_home";
 
 function App() {
   const isLoggedIn = false; //change this after authentication done
@@ -23,14 +19,8 @@ function App() {
         {isLoggedIn ? <Navbar_2 /> : <Navbar_1 />}
       </header>
       <main className="mt-[120px]">
-        <Routes>
-          <Route path="/" element={<Unreg_home />} />
-          <Route path="/login" element={<Login />} />
-          {/* If you meant /signUp instead of /signup, change the path accordingly */}
-          <Route path="/signup" element={<Signup />} />
-          {/* Redirect to home page if no matching route is found */}
-          <Route path="*" element={<Reg_home />} />
-        </Routes>
+        {/* <Main_image /> */}
+        <Signup />
       </main>
     </div>
   );
