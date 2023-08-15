451213f401b10ccab07ac8735dfa1be1849844a0
lahiru sanjana <lahirusanjana1@gmail.com>
2023-08-15 09:45:47 +0530

    npm

--- client/src/components/Add_book_for_sell.js
+++ client/src/components/Add_book_for_sell.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import Swal from 'sweetalert2'
 
 function Add_book_for_sell() {
+  const [formdata,setISBN]=useState('');
+
+  const handleIsbnChange = (e) => {
+    const updatedFormdata = { ...formdata, isbn: e.target.value };
+    setISBN(updatedFormdata);
+  };
+
   return (
     <div className="w-2/3 mx-auto shadow bg-[#FFFFFF] p-5 mb-5 rounded-md">
-      <form action="">
+      <form >
         <p className=" font-roboto text-4xl text-[#4F6D7A] my-6 font-bold">
           ADD BOOK FOR SALE
         </p>
@@ -15,8 +23,29 @@ function Add_book_for_sell() {
           className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
           placeholder="ISBN Number"
           required
+
         />
+        <button className="bg-[#BF5A36] text-white my-6 font-bold px-6 shadow-lg w-3/5"
+        onClick={(e) => {
+          e.preventDefault(); // Prevent form submission and page reload
+          // Get the value of the ISBN input field
+          
+          
+          const isbnInput = document.getElementById('isbn');
+          // const isbnValue = isbnInput.value;
+
+          if (isbnInput.value == null) {
+            console.log("HI");
+          } else {
+            // Swal.fire('Any fool can use a computer');
+          }
+              
+        }}>
+            Check!
+        </button>
         <br />
+      </form>
+      <form>
         <input
           type="text"
           className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
