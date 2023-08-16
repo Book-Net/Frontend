fb9b08626c9d37adc0e22ba8000537136f1d0a75
Samitha <rathnayakesamitha1317@gmail.com>
2023-08-16 23:50:49 +0530

    added book form

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -1,125 +1,85 @@
 import React, { useState } from "react";
-import Button from "../components/Button";
-import axios from "axios";
-import { toast } from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Button from "./Button";
+import Swal from 'sweetalert2'
 
 function BookForm() {
-  const navigate = useNavigate();
-  const [data, setData] = useState({
-    title: "",
-    description: "",
-    isbn: "",
-    quantity: "",
-    price: "",
-    img: "",
-  });
-
-  const addBook = async (e) => {
-    e.preventDefault();
-    const { title, description, isbn, quantity, price, img} = data;
-    try {
-      const response = await axios.post(
-        "http://localhost:9000/add_book", // Update the URL as needed
-        { title, description, isbn, quantity, price, img }, 
-        { withCredentials: false }
-      );
-
-      const responseData = response.data;
-      if (responseData.error) {
-        toast.error(responseData.error);
-      } else {
-        setData({
-          title: "",
-          description: "",
-          isbn: "",
-          quantity: "",
-          price: "",
-          img: "",
-        });
-        toast.success("Book added successfully!");
-        navigate("/BookList")
-        // Redirect logic or handle success as needed
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error("An error occurred. Please try again later.");
-    }
-  };
-
   return (
-    <div className="w-full">
-      <form action="" onSubmit={addBook} >
-        <p className="font-roboto text-4xl text-[#4F6D7A] my-6 font-bold">
-          Add a New Book
+    <div className="w-2/3 mx-auto shadow bg-[#FFFFFF] p-5 mb-5 rounded-md">
+
+      <form action="http://localhost:9000/add_book" method="post" encType="multipart/form-data">
+        <p className=" font-roboto text-4xl text-[#4F6D7A] my-6 font-bold">
+          ADD BOOK
         </p>
+        <br />
         <input
+          name="isbn"
           type="text"
-          className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
-          placeholder="Title"
+          className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] text-[#BF5A36] w-3/5"
+          placeholder="ISBN Number"
           required
-          value={data.title}
-          onChange={(e) =>
-            setData((prevData) => ({ ...prevData, title: e.target.value }))
-          }
+        />
+        <br />
+      
+        <input
+          name='title'
+          type="text"
+          className="pl-2 py-3 text-[#BF5A36] shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
+          placeholder="Name of the Book"
+          required
         />
         <br />
         <input
+        name='author'
           type="text"
-          className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
-          placeholder="Description"
+          className="pl-2 py-3 text-[#BF5A36] shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
+          placeholder="Author"
           required
-          value={data.description}
-          onChange={(e) =>
-            setData((prevData) => ({ ...prevData, description: e.target.value }))
-          }
         />
         <br />
         <input
-          type="text"
-          className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
-          placeholder="ISBN"
-          required
-          value={data.isbn}
-          onChange={(e) =>
-            setData((prevData) => ({ ...prevData, isbn: e.target.value }))
-          }
-        />
-        <input
-          type="text"
-          className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
+          name='quantity'
+          type="number"
+          className="pl-2 py-3 text-[#BF5A36] shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
           placeholder="Quantity"
           required
-          value={data.quantity}
-          onChange={(e) =>
-            setData((prevData) => ({ ...prevData, quantity: e.target.value }))
-          }
         />
+        <br />
         <input
-          type="text"
-          className="pl-2 py-3 shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
+          name='price'
+          type="number"
+          className="pl-2 py-3 text-[#BF5A36] shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
           placeholder="Price"
           required
-          value={data.price}
-          onChange={(e) =>
-            setData((prevData) => ({ ...prevData, price: e.target.value }))
-          }
         />
+        <br />
+             
         <input
-  type="file"
-  accept="image/*"
-  className="my-3"
-  required
-  onChange={(e) =>
-    setData((prevData) => ({
-      ...prevData,
-      img: e.target.files[0],
-    }))
-  }
-/>
-        <br/> 
+          type="file"
+          name="file"
+          accept="image/*"
+          className="pl-2 py-3 text-[#BF5A36] shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
+          placeholder="Quantity"
+          required
+          multiple
+        />
+        <br />
+        <textarea
+          name="description"
+          id=""
+          rows="5"
+          placeholder="Description"
+          className="pl-2 py-3 text-[#BF5A36] shadow-md my-3 rounded-md placeholder-[#BF5A36] w-3/5"
+          maxLength="600"
+        ></textarea>
+
+         
+        
+        <br />
+        <br />
+
         <Button
-          className="bg-[#BF5A36] text-white mt-3 font-bold px-6"
+          className="bg-[#BF5A36] text-white my-6 font-bold px-6 shadow-lg"
           type="submit"
         >
           Add Book
