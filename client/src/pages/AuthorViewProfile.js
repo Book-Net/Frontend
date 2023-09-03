e144332252b32cbe839c90260acf8f7fd74ec921
Naveekj <naveendrakj@gmail.com>
2023-09-04 03:27:27 +0530

    added the edit button to the about author

--- client/src/pages/AuthorViewProfile.js
+++ client/src/pages/AuthorViewProfile.js
@@ -5,6 +5,7 @@ import StarRating from "../components/StarRating";
 import Msg_Author from "../components/Msg_Author";
 import EditPopup from "../components/EditPopup";
 import AuthorView_description from "../components/AuthorView_description";
+import EditAboutAuthorPopup from "../components/EditAboutAuthorPopup";
 
 function AuthorViewProfile() {
   return (
@@ -13,7 +14,10 @@ function AuthorViewProfile() {
 
       {/* <Author_description /> */}
       <AuthorView_description/>
-      <p className="topics text-[#4F6D7A] text-2xl text-left font-medium ml-8 mt-8">About Author</p>
+      <div className="flex items-center mt-8 ml-8 text-2xl font-medium text-left topics">
+        <p className="text-[#4F6D7A] mr-4">About Author</p>
+        <EditAboutAuthorPopup/>
+      </div>
        <p className="topics text-[#BF5A36] text-[18px] text-left font-normal ml-8 mt-8">Step into the captivating world of celebrated author Samuel Hartman, a luminary in the realm of fiction. Samuel Hartman's literary journey is adorned with an array of prestigious awards and accolades, testaments to his exceptional storytelling prowess and profound impact on the literary landscape. </p>
        <p className="topics text-[#BF5A36] text-[18px] font-normal text-left  ml-8 mt-8">Qualifications and Awards <p> Master of Fine Arts in Creative Writing: University of Literature<br></br>Fictional Excellence Award</p></p>
       <p className="topics text-[#4F6D7A] text-2xl text-left font-medium ml-8 mt-8">Published Books</p>
