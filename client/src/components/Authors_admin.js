5af6a0170daf0dfd7d48dc977f319d2a264730c2
yasinthags <yasinthags99@gmail.com>
2023-10-20 02:56:59 +0530

    added icons, popups

--- client/src/components/Authors_admin.js
+++ client/src/components/Authors_admin.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React , { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Button from "./Button";
 
 function Authors_admin() {
+
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const Popup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  }; 
+
   return (
     <div >
         <div className='border-2 border-[#F1E6D9] bg-[#F2CB9E] w-4/5 h-[auto] p-4 justify-center'>
@@ -20,7 +27,7 @@ function Authors_admin() {
               <label class="col-span-3 p-5 text-xl text-left self-center">Author1</label>
               <Button
               className="bg-[#BF5A36] col-span-1 text-white mt-1 w-2/5 font-bold mr-1 p-0"
-              type="submit"
+              type="button"  onClick={Popup}
               >
               View
               </Button>
@@ -30,11 +37,52 @@ function Authors_admin() {
               <label class="col-span-3 p-5 text-xl text-left self-center">Author2</label>
               <Button
               className="bg-[#BF5A36] col-span-1 text-white mt-1 w-2/5 font-bold mr-1 p-0"
-              type="submit"
+              type="button"  onClick={Popup}
               >
               View
               </Button>
             </div>
+
+            {isPopupVisible && (
+              <div className="fixed inset-0 z-50 flex items-center justify-center">
+                <div className="absolute inset-0 bg-gray-800 opacity-50"></div>
+                <div className="relative border-[3px] border-[#BF5A36] bg-white rounded-md shadow-lg p-4">
+                  <label class="p-5 font-bold text-left text-gray-800 self-center">Author 1</label>
+                  <form >
+                     <table class="mt-5 text-left">
+                       <tr>
+                         <th>Books -</th>
+                       </tr>
+                       <tr>
+                         <td>Book1</td>
+                         <td>ISBN - 123ABCD</td>
+                       </tr>
+                       <tr>
+                          <th>Email</th>
+                          <td class="px-5">Author1@gmail.com</td>
+                       </tr>
+                       <tr>
+                         <th>Telephone</th>
+                         <td class="px-5">0775642345</td>
+                       </tr>
+                       <tr>
+                         <th>Address</th>
+                         <td class="px-5">23,Temple road, mawanella</td>
+                       </tr>
+                       
+                     </table>
+                     <Button
+                       className="bg-[#BF5A36] col-span-1 text-white mt-4 w-2/5 font-bold mr-1 p-0"
+                       type="submit" onClick={Popup}
+                       >
+                       Accept
+                     </Button>
+                  </form>
+                  <Button className="text-[#BF5A36]"onClick={Popup}>Close</Button>
+                </div>
+              </div>
+            )}
+
         </div>
     </div>
 
--- client/src/pages/Sidebar_admin.js
+++ client/src/pages/Sidebar_admin.js
@@ -3,6 +3,10 @@ import Dashboard_admin from "../components/Dashboard_admin";
 import Authors_admin from "../components/Authors_admin";
 import Transactions_admin from "../components/Transactions_admin";
 import Complaints_admin from "../components/Complaints_admin";
+import { LuLayoutDashboard } from "react-icons/lu";
+import { RiBallPenLine } from "react-icons/ri";
+import { GrTransaction } from "react-icons/gr";
+import { PiWarningBold } from "react-icons/pi";
 
 function Sidebar_admin() {
 
@@ -32,32 +36,32 @@ function Sidebar_admin() {
         <ul class="font-roboto text-xl text-[#000000] mx-auto font-semibold block bg-gray-200 h-auto;">
           
           <li>
-           <button onClick={(evt) => openCity(evt, 'dashboard')} id="defaultOpen"  className=" tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 border-2 border-[#ffffff] ${isActive('dashboard') ? 'bg-[#BF5A36] text-[#ffffff]' : ''}">
-              Dashboard
+           <button onClick={(evt) => openCity(evt, 'dashboard')} id="defaultOpen"  className=" tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 flex border-2 border-[#ffffff] ${isActive('dashboard') ? 'bg-[#BF5A36] text-[#ffffff]' : ''}">
+             <span className="ml-6 mt-1"><LuLayoutDashboard /></span> <p className="ml-6">Dashboard</p>
            </button>
           </li>
           <li>
            <button
              onClick={(evt) => openCity(evt, "authors")}
-             className="tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 border-2 border-[#ffffff]"
+             className="tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 flex border-2 border-[#ffffff]"
            >
-             Authors
+             <span className="ml-6 mt-1"><RiBallPenLine /></span> <p className="ml-6">Authors</p>
            </button>
           </li>
           <li>
            <button
              onClick={(evt) => openCity(evt, "transactions")}
-             className="tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 border-2 border-[#ffffff]"
+             className="tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 flex border-2 border-[#ffffff]"
            >
-             Transactions
+             <span className="ml-6 mt-1"><GrTransaction /></span> <p className="ml-6">Transactions</p>
            </button>
           </li>
           <li>
            <button
              onClick={(evt) => openCity(evt, "complaints")}
-             className="tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 border-2 border-[#ffffff]"
+             className="tablinks min-w-[300px] hover:bg-[#BF5A36] hover:text-[#ffffff] hover:transition hover:duration-200 px-4 py-2 flex border-2 border-[#ffffff]"
            >
-             Complaints
+             <span className="ml-6 mt-1"><PiWarningBold /></span> <p className="ml-6">Complaints</p>
            </button>
           </li>
         </ul>
