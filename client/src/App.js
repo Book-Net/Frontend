a14c8bc1fcbd470273a1a44198db4fd9b9e63040
Samitha <rathnayakesamitha1317@gmail.com>
2023-10-29 20:14:10 +0530

    few changes with assests

--- client/src/App.js
+++ client/src/App.js
@@ -15,10 +15,13 @@ import Add_book_for_bid from "./components/Add_book_for_bid";
 import Unreg_home from "./pages/Unreg_home";
 import Book_sale_details from "./pages/Book_sale_details";
 import Book_bid_details from "./pages/Book_bid_details";
-import Profile from "./pages/Profile";
+// import Profile from "./pages/Profile";
 import Edit_profile from "./pages/Edit_profile";
 import Place_bid from "./pages/Place_bid";
 import Sidebar_admin from "./pages/Sidebar_admin";
+import BookList from "./pages/BookList";
+import AddFor_Bid from "./pages/AddFor_Bid";
+
 
 function App() {
   const isLoggedIn = false; //change this after authentication done
@@ -52,6 +55,8 @@ function App() {
           <Route path="/sidebar_admin" element={<Sidebar_admin />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/bookList" element={<BookList />} />
+          <Route path="/AddFor_Bid" element={<AddFor_Bid />} />
           
         </Routes>
       </main>
