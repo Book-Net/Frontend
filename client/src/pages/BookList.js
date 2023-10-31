7f8b2814ddc78e3a45cefcc18edf384824def59f
Samitha <rathnayakesamitha1317@gmail.com>
2023-10-31 12:45:55 +0530

    Send to Lahiru

--- client/src/pages/BookList.js
+++ client/src/pages/BookList.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+
 import axios from "axios";
 import BookCard from "../components/BookCard";
 import Main_image from "../components/Main_image";
 import Searchbar from "../components/Searchbar";
+import { Link } from "react-router-dom";
 
 const BookList = () => {
+  
+  // const handleBookCardClick = async (bookId) => {
+  //   // Send a request to the backend using the bookId
+  //   await axios.get(`http://localhost:9000/BookList/:${bookId}`)
+  //     .then(response => {
+  //       // Handle the response, e.g., display book details
+  //       console.log(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error('Error fetching book details:', error);
+  //     });
+  // };
+
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
@@ -30,8 +46,9 @@ const BookList = () => {
         </div>
         <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-8">
           {books.map((book, index) => (
-            <BookCard book={book} key={index} />
+            <Link to={`/booksell_detail/${book._id}`} key={index}> <BookCard book={book} /></Link>
           ))}
+          {/* {console.log(books[0]._id)} */}
         </div>
       </div>
     </section>
--- client/src/pages/Book_sale_details.js
+++ client/src/pages/Book_sale_details.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
+
+import axios from "axios";
 import Book_categories from "../components/Book_categories";
 import book from "../assets/book1.png";
 import Searchbar from "../components/Searchbar";
@@ -7,45 +11,70 @@ import BuyNowCard from "../components/BuyNowCard";
 import Book_name_author from "../components/Book_name_author";
 import Button from "../components/Button";
 import { AiOutlineHeart } from "react-icons/ai";
+import checkoutBtn from "../components/CheckoutBtn";
+
 
 const Book_sale_details = () => {
+  const [book, setBook] = useState("");
+  const bookId = useParams();
+  console.log( bookId);
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        
+        const response = await axios.get(`http://localhost:9000/book_details/${bookId.id}`);
+        // console.log(response)
+        setBook(response.data);
+        // setBooks(response.data);
+        // console.log("response Data" );
+        
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        console.error();
+      }
+    };
+    fetchBooks();
+  }, []);
+
+
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center p-4">
-      <div className="w-3/4 mx-auto">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+      {/* <div className="w-3/4 mx-auto">
         <Searchbar />
       </div>
       <div className="w-3/4 mx-auto">
         <Book_categories />
-      </div>
-      <div className="bg-white p-20 rounded-lg shadow-md w-full md:max-w-screen-lg">
+      </div> */}
+      <div className="w-full p-20 bg-white rounded-lg shadow-md md:max-w-screen-lg">
         <div className="flex">
           <div className="w-1/3">
             <img src={book} className="w-3/5 rounded-md" alt="" />
           </div>
-          <div className="ml-0 mt-2 mr-4">
+          <div className="mt-2 ml-0 mr-4">
             <AiOutlineHeart className="text-red-400 w-7 h-7" />
           </div>
           <div className="w-2/3 px-4">
             <div className="pb-2">
-              <Book_name_author />
+              <Book_name_author title={book.title} author={book.authors}/>
             </div>
             <div className="mt-4 bg-gray-100 p-4 rounded-md shadow-md shadow-#9B9B9B w-1/2 ml-0">
-              <BuyNowCard />
+              <BuyNowCard price={book.price} cartItems={['book1']}/>
+              {/* <checkoutBtn/> */}
             </div>
           </div>
         </div>
-        <div className="mt-10 bg-gray-100 p-20 rounded-lg shadow-md w-full md:max-w-screen-lg flex justify-end">
+        <div className="flex justify-end w-full p-20 mt-10 bg-gray-100 rounded-lg shadow-md md:max-w-screen-lg">
           <Book_sale_description />
         </div>
 
         {/* <Button
-          className="mt-4 bg-gray-100 border border-red-500 hover:bg-gray-300 text-red-700 py-2 px-4 rounded shadow-md"
+          className="px-4 py-2 mt-4 text-red-700 bg-gray-100 border border-red-500 rounded shadow-md hover:bg-gray-300"
           type="submit"
         >
           Ask a question
         </Button> */}
 
-        <div className="mt-8 bg-gray-100 p-5 rounded-lg shadow-md w-full md:max-w-screen-lg">
+        <div className="w-full p-5 mt-8 bg-gray-100 rounded-lg shadow-md md:max-w-screen-lg">
           <Seller_description />
         </div>
       </div>
